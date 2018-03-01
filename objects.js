f0b183e0a6e1cc288714adecd5276c9058f2fdcf
Tien Nguyen <pri859@sudomail.com>
2018-03-01 15:42:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,8 +10,3 @@ function removeFromPlaylist(playlist, artistName) {
 delete playlist.Kanye
  return playlist
 }
-
-it('should take less than 500ms', function(done){
-  this.timeout(0);
-  setTimeout(done, 300);
-});
